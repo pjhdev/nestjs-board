9ede0098aa52a6d2a9b2d1a5c99efdf9d6da5708
pjhdev <zkdlwnfm1@nerdystar.io>
2022-09-02 13:01:31 +0900

    fix: add return null to findOneWithPasswordColumn

--- app/src/board/board.repository.ts
+++ app/src/board/board.repository.ts
@@ -51,6 +51,6 @@ export class BoardRepository extends Repository<Board> {
       .getRawOne();
 
     // getRawOne 으로 가져오는 결과는 json 데이터이기 때문에 Board entity 로 다시 변경해준다.
-    return new Board(foundBoard);
+    return foundBoard ? new Board(foundBoard) : null;
   }
 }
